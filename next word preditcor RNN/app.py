1b7d1a5252d091dbf63dd6ca1a16555cf16c7911
Fatima Mansoor <139262595+FatimaMansoor-dev@users.noreply.github.com>
2025-01-07 23:13:53 +0500

    Update app.py for 3 options instead of 1

--- next word preditcor RNN/app.py
+++ next word preditcor RNN/app.py
@@ -8,7 +8,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 app = Flask(__name__, static_url_path='/static')
 
 # Load the LSTM model
-model = load_model('text_generator/model.h5')
+model = load_model('text_generator/model.h5', compile=False)
+model.compile(loss = 'categorical_crossentropy', optimizer ='adam', metrics = ['accuracy'])
 
 # Load the tokenizer
 with open('text_generator/tokenizer.pkl', 'rb') as f:
@@ -34,13 +35,13 @@ def predict():
     # Make a prediction
     prediction = model.predict(padded_sequence)[0]
 
-    # Get the predicted word index
-    predicted_word_index = tf.argmax(prediction).numpy()
+    # Get the indices of the top 3 predicted words
+    top_3_indices = tf.argsort(prediction, direction='DESCENDING')[:3].numpy()
 
-    # Map the index back to the word using the tokenizer
-    predicted_word = tokenizer.index_word.get(predicted_word_index, '')
+    # Map the indices back to words using the tokenizer
+    top_3_words = [tokenizer.index_word.get(index, '') for index in top_3_indices]
 
-    return jsonify({'predicted_word': predicted_word})
+    return jsonify({'top_3_words': top_3_words})
 
 if __name__ == '__main__':
     app.run(debug=True)
